06546901527a7c88885be00aa8d345fe6b3a2d41
Srimathi Karunakaran <srimathikarunakaran10@gmail.com>
2025-11-20 12:57:43 +0530

    songs

--- src/Cpmponents/DisplayHome.jsx
+++ src/Cpmponents/DisplayHome.jsx
@@ -6,6 +6,7 @@ import { albumsData } from '../assets/assets';
 import { songsData } from '../assets/assets';
 import { AlbumItem } from './AlbumItem'
 import { SongItem } from './SongItem';
+import { TrandingSongs } from './TrandingSongs';
 
 export const DisplayHome = () => {
   return (
@@ -25,6 +26,15 @@ export const DisplayHome = () => {
         <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
         <div className='flex overflow-auto'>
             {songsData.map((item,index) => (<SongItem  key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
+
+    </div>
+
+
+    <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Tranding Songs</h1>
+        <div className='flex overflow-auto'>
+        {albumsData.map( (item,index) => (<TrandingSongs key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
         </div>
 
     </div>
